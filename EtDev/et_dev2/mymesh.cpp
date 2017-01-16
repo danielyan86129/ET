024d9f115f477e8bfbd84ffb3303f4938c93c636
danielyan86129 <danielyan86129@gmail.com>
2017-01-15 22:53:42 -0600

    - added a function write() in class MyMesh to handle writing .ply mesh. - changed ui to use native file browser

--- EtDev/et_dev2/mymesh.cpp
+++ EtDev/et_dev2/mymesh.cpp
@@ -37,6 +37,20 @@ MyMesh * MyMesh::read( const::std::string & filename )
 	return read( filename.c_str() );
 }
 
+void MyMesh::write( const char * _fname, const MyMesh * _m )
+{
+	string fname( _fname );
+	if ( fname.find( ".off" ) != string::npos )
+		( (TriMesh*)_m )->write( _fname );
+	else if ( fname.find( ".ply" ) != string::npos )
+		write_ply_helper( fname.c_str(), _m );
+}
+
+void MyMesh::write( const std::string & _fname, const MyMesh * _m )
+{
+	write( _fname.c_str(), _m );
+}
+
 MyMesh * MyMesh::read_ply_helper( const char* _fname )
 {
 	map<string, PlyProperty> v_props_map;
@@ -91,4 +105,74 @@ MyMesh * MyMesh::read_ply_helper( const char* _fname )
 		mesh->faces.push_back( TriFace( f.verts ) );
 	}
 	return mesh;
+}
+
+bool MyMesh::write_ply_helper( const char * _fname, const MyMesh * _m )
+{
+	struct Vertex
+	{
+		float x; float y; float z;
+		unsigned char r, g, b;
+		float s; // measure
+	};
+	struct Edge
+	{
+		int v1; int v2;
+		unsigned char r, g, b;
+		float s; // measure
+	};
+	struct Face
+	{
+		unsigned char nvts;
+		int verts[ 3 ];
+		unsigned char r, g, b;
+		float s; // measure
+	};
+	std::map<std::string, PlyProperty> vert_props;
+	vert_props[ "x" ] = { "x", Float32, Float32, offsetof( Vertex, x ), PLY_SCALAR, 0, 0, 0 };
+	vert_props[ "y" ] = { "y", Float32, Float32, offsetof( Vertex, y ), PLY_SCALAR, 0, 0, 0 };
+	vert_props[ "z" ] = { "z", Float32, Float32, offsetof( Vertex, z ), PLY_SCALAR, 0, 0, 0 };
+	std::map<std::string, PlyProperty> edge_props;
+	edge_props[ "vertex1" ] = { "vertex1", Int32, Int32, offsetof( Edge, v1 ), PLY_SCALAR, 0, 0, 0 };
+	edge_props[ "vertex2" ] = { "vertex2", Int32, Int32, offsetof( Edge, v2 ), PLY_SCALAR, 0, 0, 0 };
+	std::map<std::string, PlyProperty> face_props;
+	face_props[ "vertex_indices" ] = {
+		"vertex_indices", Int32, Int32, offsetof( Face, verts ),
+		PLY_LIST, Uint8, Uint8, offsetof( Face,nvts ) };
+	vector<Vertex> output_vts( _m->vertices.size() );
+	vector<Edge> output_edges( _m->lines.size() );
+	vector<Face> output_faces( _m->faces.size() );
+	// fill in the data to output lists (face might contain color)
+	for ( auto i = 0; i < output_vts.size(); ++i )
+	{
+		auto& o_p = output_vts[ i ];
+		o_p.x = _m->vertices[ i ][ 0 ];
+		o_p.y = _m->vertices[ i ][ 1 ];
+		o_p.z = _m->vertices[ i ][ 2 ];
+	}
+	for ( auto i = 0; i < output_edges.size(); ++i )
+	{
+		auto& o_e = output_edges[ i ];
+		o_e.v1 = _m->lines[ i ][ 0 ];
+		o_e.v2 = _m->lines[ i ][ 1 ];
+	}
+	for ( auto i = 0; i < output_faces.size(); ++i )
+	{
+		auto& o_f = output_faces[ i ];
+		o_f.nvts = 3;
+		o_f.verts[ 0 ] = _m->faces[ i ][ 0 ];
+		o_f.verts[ 1 ] = _m->faces[ i ][ 1 ];
+		o_f.verts[ 2 ] = _m->faces[ i ][ 2 ];
+	}
+
+	std::cout << "writing to ply file... " << std::endl;
+	std::cout << "# vts/edges/faces: "
+		<< output_vts.size() << "/"
+		<< output_edges.size() << "/"
+		<< output_faces.size() << std::endl;
+
+	ply::PLYwriter ply_writer;
+	return ply_writer.write( _fname, true, true, true,
+		vert_props, edge_props, face_props,
+		output_vts, output_edges, output_faces ) == ply::SUCCESS;
 }
