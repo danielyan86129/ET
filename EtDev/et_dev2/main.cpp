accf0afd3fe1e524b2ba878c7fd951e6c510e39c
yajieyan <danielyan86129@hotmail.com>
2017-04-13 18:47:58 -0500

    Added back the "run_all" cmd option.

--- EtDev/et_dev2/main.cpp
+++ EtDev/et_dev2/main.cpp
@@ -20,6 +20,7 @@ DEFINE_int32( burn_sch, 1, "the burning scheme (0: orig-and-steiner, 1: steiner-
 
 DEFINE_string( mc_msure, "null", "output specified measure on medial curves. nothing to output by default. \
 							valid value: {shape-diam, shape-width, ...}. OPTIONAL." );
+DEFINE_bool( run_all, false, "compute all the way to skeleton generation." );
 DEFINE_bool( export_skel, false, "compute all the way to curve skeleton generation & output skel files." );
 DEFINE_double( theta_2, 1.1, "skeleton threshold for face pruning. Assume unit bounding box. All faces purged when > 1. OPTIONAL." );
 DEFINE_double( theta_1, 0.05, "skeleton threshold for curve pruning. Assume unit bounding box. All curves purged when > 1. OPTIONAL." );
@@ -51,11 +52,12 @@ int main(int argc, char *argv[])
 		w.onCleanTopoBtnClicked(); // check if there is closed pockets
 		w.onExportETBtnClicked(); // export ET per-vertex on ma to a file
 		
-		if ( FLAGS_export_skel )
+		if ( FLAGS_export_skel || FLAGS_run_all )
 		{
 			w.onCreateHSClicked(); // generate skeleton
 			w.onVisHSClicked(); // prune skeleton
-			w.onExportHSBtnClicked(); // export skeleton
+			if ( FLAGS_export_skel )
+				w.onExportHSBtnClicked(); // export skeleton
 		}
 		else if ( !google::GetCommandLineFlagInfoOrDie( "mc_msure" ).is_default )
 			w.onDualizeClicked();
